dbda660850e31d574ee25c91fd0f7a0dd4f03a61
RahulBansal <rahulbansal2004@gmail.com>
2024-03-30 11:44:57 -0400

    basic camera file for backend testing

--- Backend/camera.py
+++ Backend/camera.py
@@ -7,8 +7,6 @@ while cap.isOpened():
     if not success:
         continue
     image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    print("test")
     cv2.imshow("yooooo", cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
