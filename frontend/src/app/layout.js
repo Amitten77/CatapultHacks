893730860631d517f0c96fc5436e324345ed3d34
Dylan Subramanian <dylansubramanian@gmail.com>
2024-03-30 12:35:41 -0400

    Added hero page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Tourney } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const tourney = Tourney({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={tourney.className}>{children}</body>
     </html>
   );
 }
